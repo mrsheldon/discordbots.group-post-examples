caf7242a071faffc3a5e9f8732207e7db2a55134
DetectiveHuman <jacob.rayner@outlook.com>
2018-09-03 14:50:59 +0100

    Updated content type

--- JDA/Kotlin/noShards.kt
+++ JDA/Kotlin/noShards.kt
@@ -2,7 +2,7 @@ val url = "https://discordbots.group/api/bot/${jda.selfuser.id}"
 val client = OkHTTPClient()
 val data : JSONObject = JSONObject()
 data.put("count", jda.guilds.size())
-val body : RequestBody = RequestBody.create(MediaType.parse("application/x-www-form-urlencoded"), data.toString());
+val body : RequestBody = RequestBody.create(MediaType.parse("application/json"), data.toString());
 
 val request : Request = RequestBuilder()
     .url(url)
