620ab16d2d76b1773fc98f000bbfe8b80f478009
DetectiveHuman <jacob.rayner@outlook.com>
2018-09-03 14:52:56 +0100

    Update noShards.cs

--- CSharpPlus/noShards.cs
+++ CSharpPlus/noShards.cs
@@ -2,7 +2,7 @@ HttpClient client = new HttpClient();
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Post, "https://discordbots.org/api/bots/<BotID>/stats");
             string json = JsonConvert.SerializeObject(new { count = e.Client.Guilds.Count });
             request.Content = new StringContent(json);
-            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/x-www-form-urlencoded");
+            request.Content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
             request.Headers.Add("Authorization", "Token");
             HttpResponseMessage message = await client.SendAsync(request);
             client.Dispose();
