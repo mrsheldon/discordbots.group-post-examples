cd58828210e658bcc9c6ea6f49aa3203bef320ec
DetectiveHuman <jacob.rayner@outlook.com>
2018-09-03 14:52:27 +0100

    Update noShards.cs

--- Discord.NET/noShards.cs
+++ Discord.NET/noShards.cs
@@ -2,7 +2,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using (var webclient = new HttpClient())
-using (var content = new StringContent($"{{ \"count\": {client.Guilds.Count}}}", Encoding.UTF8, "application/x-www-form-urlencoded"))
+using (var content = new StringContent($"{{ \"count\": {client.Guilds.Count}}}", Encoding.UTF8, "application/json"))
 {
     webclient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("token");
     HttpResponseMessage response = await webclient.PostAsync("https://discordbots.group/api/bot/YOUR_BOT_ID_HERE", content);
