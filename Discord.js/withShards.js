22f1c8e44f8cae4ba9153a3ff8ce0c22abc1d70c
DetectiveHuman <jacob.rayner@outlook.com>
2018-11-10 18:47:37 +0000

    Updated code.

--- Discord.js/withShards.js
+++ Discord.js/withShards.js
@@ -3,5 +3,10 @@ client.shard.fetchClientValues('guilds.size')
   var totalGuilds = results.reduce((prev, val) => prev + val, 0);
 const DBG = require('discordbots.group');
 const DBGClient = new DBG(`${client.user.id}`, 'TOKEN_HERE');
-DBGClient.updateServerCount(totalGuilds);
+  
+DBGClient.updateCount(totalGuilds).then(() => {
+  console.log('Successfully updated server count.')
+}).catch((e) => {
+  console.error(e)
+})
 })
